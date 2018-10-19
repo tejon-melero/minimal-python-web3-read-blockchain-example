871b88fa7611c38c2424d834f4b8bbc8e8abd7f3
VG <31324402+VGCif@users.noreply.github.com>
2018-10-19 10:33:05 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,6 +6,4 @@ NETWORK_TO_USE = 'local'  # choose between 'local' --> ganache (use this if you 
 # the local blockchain with truffle develop) 'in_memory_test_rpc' --> simple in memory blockchain (use this if you are
 # not using truffle to start the development blockchain)  or 'rinkeby' --> rinkeby testnet
 CONTRACTS_FOLDER = contracts_folder = os.path.join(os.path.join(project_directory, 'truffle_project'), 'contracts')
-ETHER_WALLET_ID_TO_USE = 0
-
-
+ETHER_WALLET_ID_TO_USE = 0 #insert your wallet
